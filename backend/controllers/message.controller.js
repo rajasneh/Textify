0d2e33661aa8188542e199519b1077bc3c15fc5d
Asneh Raj <rajasneh91@gmail.com>
2025-02-09 20:12:29 +0530

    Message controller

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,60 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/Cloudinary.js";
+
+export const getUserForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    console.log("Error in getUserForSidebar: ", err.message);
+    res.status(500).json({ err: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, reciverId: userToChatId },
+        { senderId: userToChatId, reciverId: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    console.log("Error in getMessages controller", err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: reciverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      reciverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
+
 import {connectDB} from "../lib/db.js"
+
 import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
 
 dotenv.config();
+
 const app = express();
+
 const PORT = process.env.PORT;
 
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
