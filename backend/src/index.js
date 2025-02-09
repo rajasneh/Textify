9f5a098123633df9e2bc68765e20433963ae6272
Asneh Raj <rajasneh91@gmail.com>
2025-02-09 19:14:49 +0530

    Auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import {connectDB} from "../lib/db.js"
 import authRoutes from "../routes/auth.route.js";
 
@@ -8,9 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
