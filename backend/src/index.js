2a565874c25fe6d7a7462381047861878b35f824
Asneh Raj <rajasneh91@gmail.com>
2025-02-15 22:58:59 +0530

    Ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
-import helmet from "helmet";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
