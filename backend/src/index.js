2644fccccc75bba1f2335d18dd3e182d364e63e6
Asneh Raj <rajasneh91@gmail.com>
2025-02-15 18:42:51 +0530

    Ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,29 +2,38 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {connectDB} from "../lib/db.js"
+import { connectDB } from "../lib/db.js";
 import bodyParser from "body-parser";
 import authRoutes from "../routes/auth.route.js";
 import messageRoutes from "../routes/message.route.js";
-import { app ,server} from "../lib/socket.js";
+import { app, server } from "../lib/socket.js";
 
+import path from "path";
 dotenv.config();
 
-
 const PORT = process.env.PORT;
-app.use(bodyParser.json({ limit: '10mb' })); // Example for 10MB limit
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+const __dirname = path.resolve();
+app.use(bodyParser.json({ limit: "10mb" })); // Example for 10MB limit
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
   connectDB();
