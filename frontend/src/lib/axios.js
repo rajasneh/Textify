3782c1380c62c6f8b38fded0c8a60d8af45db4fa
Asneh Raj <rajasneh91@gmail.com>
2025-02-15 18:37:01 +0530

    Ready for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL:
+    import.meta.env.MODE === "devlopment"
+      ? "http://localhost:5001/api"
+      : "/api",
   withCredentials: true,
 });
