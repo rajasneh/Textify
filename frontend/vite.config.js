f77d926a3a51e0e5fc4e62365bf50ffc81e90b58
Asneh Raj <rajasneh91@gmail.com>
2025-02-15 20:30:30 +0530

    Ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2000,  // Increase the chunk size limit to 2 MB (2000 KB)
-  },
-});
+})
