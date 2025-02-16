66bcaddf9d723312ed7bd9bd4c43e1448fa7e29b
Asneh Raj <rajasneh91@gmail.com>
2025-02-16 10:50:20 +0530

    init

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 import server from "../enviroment"; // ✅ Import the server constant
 
 export const axiosInstance = axios.create({
-  baseURL: `${server}/api`, // ✅ Uses the same server URL
+  baseURL: "https://textifybackend-9z34.onrender.com", // ✅ Uses the same server URL
   withCredentials: true, // Required for authentication
 });
