8b43a4bdb100bd2b87fbc7f4d1ac8099c6c53944
Asneh Raj <rajasneh91@gmail.com>
2025-02-16 09:25:08 +0530

    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,18 +29,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"), (err) => {
-      if (err) {
-        res.status(500).send(err);
-      }
-    });
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
