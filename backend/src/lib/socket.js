07f6a0edd97fed8c54833fc852001721828614b9
Asneh Raj <rajasneh91@gmail.com>
2025-02-16 10:54:41 +0530

    Ready for deployment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,10 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173", // Keep for local development
-      "https://textify-1g07.onrender.com", // ✅ Replace with your deployed frontend URL
-    ],
+    origin: ["https://textifybackend-9z34.onrender.com"],
   },
 });
 
